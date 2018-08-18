0beeb18cc12adbe86bff759ecede3818bee63cd3
Pedro Gomes <pedrogomes94@gmail.com>
2018-08-18 12:24:18 +0100

    change certificate

--- src/model/walletconnect.js
+++ src/model/walletconnect.js
@@ -4,40 +4,45 @@ import * as ethWallet from './ethWallet';
 import personalData from './personalData';
 
 export const walletConnectInstance = {
-  walletWebhook: 'https://walletconnect.balance.io/webhook/push-notify',
+  pushEndpoint: 'https://walletconnect.balance.io/webhook/push-notify',
   fcmToken: null,
   walletConnector: null,
 };
 
 export const walletConnectInit = async (bridgeUrl, sessionId, sharedKey, dappName) => {
-    const fcmToken = await FCM.getFCMToken();
-    const walletConnector = new RNWalletConnect({ sessionId, sharedKey, dappName, bridgeUrl});
-    walletConnectInstance.walletConnector = walletConnector;
-    walletConnectInstance.fcmToken = fcmToken;
+  const fcmToken = await FCM.getFCMToken();
+  const walletConnector = new RNWalletConnect({
+    sessionId,
+    sharedKey,
+    dappName,
+    bridgeUrl,
+  });
+  walletConnectInstance.walletConnector = walletConnector;
+  walletConnectInstance.fcmToken = fcmToken;
 };
 
 export const walletConnectSendSession = async () => {
-    const address = await ethWallet.loadAddress();
+  const address = await ethWallet.loadAddress();
   try {
-      await walletConnectInstance.walletConnector.sendSessionStatus({
-	  fcmToken: walletConnectInstance.fcmToken,
-	  walletWebhook: walletConnectInstance.walletWebhook,
-	  data: { address, personalData },
-      });
+    await walletConnectInstance.walletConnector.sendSessionStatus({
+      fcmToken: walletConnectInstance.fcmToken,
+      pushEndpoint: walletConnectInstance.pushEndpoint,
+      data: { address, personalData },
+    });
   } catch (err) {
-      console.log('send session status error', err);
+    console.log('send session status error', err);
   }
 };
 
 export const walletConnectGetTransaction = async transactionId => {
-    const transactionData = await walletConnectInstance.walletConnector.getTransactionRequest(transactionId);
-    return transactionData;
+  const transactionData = await walletConnectInstance.walletConnector.getTransactionRequest(transactionId);
+  return transactionData;
 };
 
 export const walletConnectSendTransactionHash = async (transactionId, success, txHash) => {
-    try {
+  try {
     await walletConnectInstance.walletConnector.sendTransactionStatus(transactionId, { success, txHash });
-    } catch (err) {
-	console.log('sending txn status error', err);
-    }
+  } catch (err) {
+    console.log('sending txn status error', err);
+  }
 };
