494e4a743d55dc6166b1636e39d449effd32f4cd
Pedro Gomes <pedrogomes94@gmail.com>
2018-08-21 18:21:47 +0100

    new redux folder

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'remote-redux-devtools';
+import reducers from '.';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(ReduxThunk)));
+
+export default store;
