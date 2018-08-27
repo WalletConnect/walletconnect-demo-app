f7032665c603b24eb8e0997139e516554f60995b
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-08-27 18:49:01 +0300

    initial build building

--- android/app/src/main/java/com/walletconnect/MainApplication.java
+++ android/app/src/main/java/com/walletconnect/MainApplication.java
@@ -1,12 +1,11 @@
 package com.walletconnect;
+import android.support.annotation.NonNull;
 
-import android.app.Application;
-
-import com.facebook.react.ReactApplication;
-import com.reactnativenavigation.NavigationReactPackage;
-import com.facebook.react.ReactNativeHost;
+//import com.facebook.react.ReactApplication;
+//import com.reactnativenavigation.NavigationReactPackage;
+//import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+//import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
@@ -22,21 +21,14 @@ import com.reactnativenavigation.NavigationApplication;
          return BuildConfig.DEBUG;
      }
 
-     protected List<ReactPackage> getPackages() {
-         // Add additional packages you require here
-         // No need to add RnnPackage and MainReactPackage
-         return Arrays.<ReactPackage>asList(
-             // eg. new VectorIconsPackage()
-         );
-     }
 
+     @NonNull
      @Override
      public List<ReactPackage> createAdditionalReactPackages() {
-         return getPackages();
-     }
-
-     @Override
-    public String getJSMainModuleName() {
-        return "index";
-    }
+	 // Add the packages you require here.
+	 // No need to add RnnPackage and MainReactPackage
+	 return Arrays.<ReactPackage>asList(
+					    // eg. new VectorIconsPackage()					    
+					      );
+     }     
  }
