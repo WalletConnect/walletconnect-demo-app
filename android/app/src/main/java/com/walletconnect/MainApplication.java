a0ac669b5ed83b379018c905c7a394828d85d4aa
Pedro Gomes <pedrogomes94@gmail.com>
2018-08-27 16:01:02 +0100

    test android

--- android/app/src/main/java/com/walletconnect/MainApplication.java
+++ android/app/src/main/java/com/walletconnect/MainApplication.java
@@ -3,16 +3,7 @@ package com.walletconnect;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.rnfingerprint.FingerprintAuthPackage;
-import com.tradle.react.UdpSocketsModule;
-import com.peel.react.TcpSocketsModule;
-import com.peel.react.rnos.RNOSModule;
 import com.reactnativenavigation.NavigationReactPackage;
-import org.reactnative.camera.RNCameraPackage;
-import com.evollu.react.fcm.FIRMessagingPackage;
-import com.bitgo.randombytes.RandomBytesPackage;
-import com.oblador.keychain.KeychainPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -21,45 +12,31 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+import com.reactnativenavigation.NavigationApplication;
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+ public class MainApplication extends NavigationApplication {
+
+     @Override
+     public boolean isDebug() {
+         // Make sure you are using BuildConfig from your own application
+         return BuildConfig.DEBUG;
+     }
+
+     protected List<ReactPackage> getPackages() {
+         // Add additional packages you require here
+         // No need to add RnnPackage and MainReactPackage
+         return Arrays.<ReactPackage>asList(
+             // eg. new VectorIconsPackage()
+         );
+     }
+
+     @Override
+     public List<ReactPackage> createAdditionalReactPackages() {
+         return getPackages();
+     }
+
+     @Override
+    public String getJSMainModuleName() {
+        return "index";
     }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SplashScreenReactPackage(),
-            new FingerprintAuthPackage(),
-            new UdpSocketsModule(),
-            new TcpSocketsModule(),
-            new RNOSModule(),
-            new NavigationReactPackage(),
-            new RNCameraPackage(),
-            new FIRMessagingPackage(),
-            new RandomBytesPackage(),
-            new KeychainPackage()
-      );
-    }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
-}
+ }
