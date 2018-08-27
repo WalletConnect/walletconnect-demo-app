aeb1c99705d8c20a6ed837c3b3c7635615434c31
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-08-27 18:54:37 +0300

    react-random-bytes added

--- android/app/src/main/java/com/walletconnect/MainApplication.java
+++ android/app/src/main/java/com/walletconnect/MainApplication.java
@@ -1,12 +1,11 @@
 package com.walletconnect;
 import android.support.annotation.NonNull;
 
-//import com.facebook.react.ReactApplication;
-//import com.reactnativenavigation.NavigationReactPackage;
-//import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-//import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.bitgo.randombytes.RandomBytesPackage;
+
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +27,8 @@ import com.reactnativenavigation.NavigationApplication;
 	 // Add the packages you require here.
 	 // No need to add RnnPackage and MainReactPackage
 	 return Arrays.<ReactPackage>asList(
-					    // eg. new VectorIconsPackage()					    
+					    // eg. new VectorIconsPackage()
+					    new RandomBytesPackage()					    
 					      );
      }     
  }
