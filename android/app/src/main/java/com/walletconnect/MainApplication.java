be61269abcb54ee2fc7f0b08e440035e24a0aaf5
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-08-28 20:26:18 +0300

    react-native-firebase for iOS basic module added

--- android/app/src/main/java/com/walletconnect/MainApplication.java
+++ android/app/src/main/java/com/walletconnect/MainApplication.java
@@ -3,6 +3,7 @@ package com.walletconnect;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.tradle.react.UdpSocketsModule;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new SplashScreenReactPackage(),
             new FingerprintAuthPackage(),
             new UdpSocketsModule(),
--- ios/WalletConnect/AppDelegate.m
+++ ios/WalletConnect/AppDelegate.m
@@ -8,15 +8,15 @@
  */
 
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import "RCCManager.h"
-#import "RNFIRMessaging.h"
+//#import "RNFIRMessaging.h"
 #import "SplashScreen.h"
 
-@interface AppDelegate() <FIRMessagingDelegate, UNUserNotificationCenterDelegate>
-@end
+// @interface AppDelegate() <FIRMessagingDelegate, UNUserNotificationCenterDelegate>
+// @end
 
 @implementation AppDelegate
 
@@ -25,9 +25,10 @@
   // Push Notifications
   //
   [FIRApp configure];
-  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-  [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionBadge | UNAuthorizationOptionSound) completionHandler:^(BOOL granted, NSError * _Nullable error) {}];
-  [application registerForRemoteNotifications];
+
+  // [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+  // [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionBadge | UNAuthorizationOptionSound) completionHandler:^(BOOL granted, NSError * _Nullable error) {}];
+  // [application registerForRemoteNotifications];
   
   // React Native
   //
@@ -47,27 +48,27 @@
   return YES;
 }
 
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-  [RNFIRMessaging willPresentNotification:notification withCompletionHandler:completionHandler];
-}
+// - (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+//   [RNFIRMessaging willPresentNotification:notification withCompletionHandler:completionHandler];
+// }
 
-#if defined(__IPHONE_11_0)
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
-  [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
-}
-#else
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler {
-  [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
-}
-#endif
+// #if defined(__IPHONE_11_0)
+// - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler {
+//   [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
+// }
+// #else
+// - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler {
+//   [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
+// }
+// #endif
 
-//You can skip this method if you don't want to use local notification
--(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-  [RNFIRMessaging didReceiveLocalNotification:notification];
-}
+// //You can skip this method if you don't want to use local notification
+// -(void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+//   [RNFIRMessaging didReceiveLocalNotification:notification];
+// }
 
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler {
-  [RNFIRMessaging didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
+// - (void)application:(UIApplication *)application didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler {
+//   [RNFIRMessaging didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+// }
 
 @end
