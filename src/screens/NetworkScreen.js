f7ae7baded1fcf48a730113fe359fed8d02121db
Mikhail Dobrokhvalov <m.dobrokhvalov@gmail.com>
2018-08-29 15:39:38 +0300

    Switch Network feature added

--- src/screens/NetworkScreen.js
+++ src/screens/NetworkScreen.js
@@ -7,26 +7,56 @@ import Section from '../components/Section';
 import Label from '../components/Label';
 import Text from '../components/Text';
 import Button from '../components/Button';
+import { capitalize } from '../helpers/utilities';
+import { List, ListItem } from 'react-native-elements';
+import { accountUpdateNetwork } from '../redux/_account';
 
 
 class NetworkScreen extends Component {
-  render() {
+    render() {
+	const { currentNetwork } = this.props;
+	
+	const networks = [
+	    "mainnet",	    
+	    "ropsten",
+	    "rinkeby",
+	    "koval"
+	];
     return (
       <Container>
-        <Card>
-          <Section>
-            <Label>{'Network'}</Label>
-            <Text>{'Account 1'}</Text>
-          </Section>
-          <Section>
-            <Label>{'Seed Words'}</Label>
-            <Button onPress={() => {}}>{'Backup'}</Button>
-          </Section>
+            <Card>
+	    <List containerStyle={{marginTop: 0}}>
+	    {
+		networks.map((n) => (		    
+			<ListItem
+		    key={n}
+		    title={capitalize(n)}
+		    hideChevron
+		    leftIcon={(n === currentNetwork) ? {name: 'check'} : null }
+		    onPress={() => {
+			this.props.accountUpdateNetwork(n);
+		    }}
+			/>
+		))
+	    }
+	</List>
         </Card>
       </Container>
     );
   }
 }
 
+NetworkScreen.propTypes = {
+  currentNetwork: PropTypes.any.isRequired,
+};
 
-export default NetworkScreen;
+const reduxProps = ({ account }) => ({
+  currentNetwork: account.network,
+});
+
+export default connect(
+  reduxProps,
+  { accountUpdateNetwork }
+)(NetworkScreen);
+
+
